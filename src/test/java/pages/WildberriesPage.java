4c8f11118d06cb4be2a8cc4bc6582ee2bfa93f97
AOvsyannikov <aovsyannikov@it-one.ru>
2024-06-05 22:18:13 +0300

    added WildberriesPage.java

---
+++ src/test/java/pages/WildberriesPage.java
@@ -0,0 +1,92 @@
+package pages;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.WebDriverConditions.currentFrameUrl;
+
+public class WildberriesPage {
+
+    private final SelenideElement
+            sellerButton = $(byTagAndText("a", "Продавайте на Wildberries")),
+            workButton = $(byTagAndText("a", "Работа в Wildberries")),
+            basket = $(".j-item-basket"),
+            mainPageButton = $(byTagAndText("a", "Перейти на главную")),
+            addresButton = $(".navbar-pc__icon--address"),
+            deliveryBannerTitle =  $(".delivery-banner-title"),
+            loginButton =  $(".j-main-login"),
+            loginTitle = $(byTagAndText("h2","Войти или создать профиль"));
+
+
+    @Step("Открытие страницы")
+    public WildberriesPage openPage(){
+        open("");
+        return this;
+    }
+
+    @Step("Переход на страницу продавцов")
+    public WildberriesPage sellerPage(){
+        sellerButton.click();
+        return this;
+    }
+
+    @Step("Проверка url")
+    public WildberriesPage CheckNewInsetUrl(String value){
+        webdriver().driver().switchTo().window(1);
+        webdriver().shouldHave(currentFrameUrl(value));
+        return this;
+    }
+
+    @Step("Переход на страницу трудоустройства")
+    public WildberriesPage workPage(){
+        workButton.click();
+        return this;
+    }
+
+    @Step("Проверка url")
+    public WildberriesPage checkUrl(String value){
+        webdriver().shouldHave(currentFrameUrl(value));
+        return this;
+    }
+
+    @Step("Переход в корзину")
+    public WildberriesPage goToBasket(){
+        basket.click();
+        return this;
+    }
+
+    @Step("Переход на главную страницу")
+    public WildberriesPage goToMainPage(){
+        mainPageButton.click();
+        return this;
+    }
+
+    @Step("Переход к доставке")
+    public WildberriesPage goToAddres(){
+        addresButton.click();
+        return this;
+    }
+
+    @Step("Проверка информации по доставке")
+    public WildberriesPage testAddresPage(String value){
+        deliveryBannerTitle.shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Нажать на кнопку Войти")
+    public WildberriesPage loginButtonClick(){
+        loginButton.click();
+        return this;
+    }
+
+    @Step("Проверка заголовка окна авторизации")
+    public WildberriesPage checkLoginPage(String value){
+        loginTitle.shouldHave(text(value));
+        return this;
+    }
+
+
+
+}
